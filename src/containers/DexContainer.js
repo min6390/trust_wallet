946506e3c468396a5fc3d38ec9db23667e60aa56
min20 <min20071996@gmail.com>
2020-11-30 14:59:35 +0700

    first

---
+++ src/containers/DexContainer.js
@@ -0,0 +1,14 @@
+import React,{} from 'react';
+import {
+    StyleSheet,
+} from 'react-native';
+import HomeContainer from './HomeContainer';
+
+
+function DexContainer() {
+    return(
+        <>
+        </>
+    )
+}
+export default DexContainer
